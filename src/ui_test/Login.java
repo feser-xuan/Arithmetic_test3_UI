4c660af69f8b23cef7a0891a5f4f78a7b9f6a097
feser-xuan <1938368539@qq.com>
2018-04-03 18:21:11 +0800

    最后提交

--- src/ui_test/Login.java
+++ src/ui_test/Login.java
@@ -1,92 +1,105 @@
 package ui_test;
 
-import java.awt.*;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*; 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+
 
 public class Login {
-	public Login()
-	{
-	JFrame frame = new JFrame("用户注册");
-    
-    //窗体基本属性
-    frame.setSize(600, 400);
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    
-    //设置主面板，相当于外层div
-    JPanel jp = new JPanel(new BorderLayout());
-    
-    //东西南北中，面板
-    JPanel jp1 = new JPanel(new FlowLayout());
-    JPanel jp2 = new JPanel(new FlowLayout());
-    JPanel jp3 = new JPanel(new GridLayout(10,2));
-    JPanel jp4 = new JPanel(new FlowLayout());
-    JPanel jp5 = new JPanel(new FlowLayout());
-    
-    //加入主面板
-    jp.add(BorderLayout.NORTH,jp1);
-    jp.add(BorderLayout.SOUTH,jp2);
-    jp.add(BorderLayout.CENTER,jp3);
-    jp.add(BorderLayout.EAST,jp4);
-    jp.add(BorderLayout.WEST,jp5);
-    
 
-    //标题
-    JLabel jl1 = new JLabel("用户注册",SwingConstants.CENTER);
-    jp1.add(jl1);
-    
-    //中间面板
-    JButton jb2= new JButton("Btn");
-    JTextField jt = new JTextField("usrname");
-    JButton jb1 = new JButton("abc");    
-    
-    jp3.add(jt);
-    jp3.add(jb1);
-    jp3.add(jb2);
-    
-            
-    //this.container = frame.getContentPane();
-   // this.container.setBackground(Color.white);
-   // this.container.add(jp);
-}
-	
-	/*
-	Button login = new Button("Login in");
-	Button exit = new Button("Login out");
-	Button register = new Button("Register");
-	JLabel input_name= new JLabel("用户名：");
-	JLabel input_password = new JLabel("密 码：");
-	JTextField in_name = new JTextField();
-	JTextField in_password = new JTextField();
-	public Login()
+	public Login() 
 	{
-	  JFrame  f = new JFrame("一个简单窗口");
-      f.setLocation(300,300);          
-      f.setSize(300,200);           
-      //f.setResizable(false);   
-      f.setUndecorated(true);
-      f.setVisible(true);
-      //f.setLayout(new FlowLayout());
-      //f.setLayout(new GridLayout(2,3,120,20));
-      JPanel jp = new JPanel(new BorderLayout());
-      f.setLayout(null);
-      in_name.setSize(200, 240);
-      in_password.setSize(200,240);
-      input_name.setBounds(10, 10, 20, 50);
-      in_name.setBounds(arg0, arg1, arg2, arg3);
-      f.add(input_name);
-      f.add(in_name);
-      f.add(input_password);
-      f.add(in_password);
-      f.add(login);
-      f.add(exit);
-      f.add(register);
-      
-     
-     // f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+		// TODO Auto-generated method stub
+		JFrame f=new JFrame();
+		f.setTitle("小学生四则运算答题系统");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		f.setVisible(true);
+		
+		//设置窗口的大小和位置
+		f.setSize(400,400);
+		f.setLocation(200,200);
+		
+		Container con=f.getContentPane();//生成一个容器	
+		con.setLayout(new GridLayout(7,1));
+		
+		JPanel pan1=new JPanel();//生成一个新的版面
+		JLabel title=new JLabel("欢迎登陆小学生四则运算答题系统");
+		title.setFont(new Font("宋体",Font.BOLD, 20));
+		pan1.add(title);
+		con.add(pan1);
+		//最上面的登陆文字
+		
+		JPanel pan2=new JPanel();//生成一个新的版面
+		JLabel name=new JLabel("用户名");
+		pan2.add(name);
+		TextField tf_name=new TextField(20);
+		pan2.add(tf_name);
+		con.add(pan2);
+		//用户名及其文本框放置在第二个版面上
+		
+		
+		JPanel pan3=new JPanel();//生成一个新的版面
+		JLabel pass = new JLabel("密码");
+		pan3.add(pass);
+		TextField password=new TextField(14);
+		password.setEchoChar('*');
+		pan3.add(password);
+		con.add(pan3);
+		//密码及其密码域放在第三个版面上
+		
+		JPanel pan4 = new JPanel();  
+		JButton b_log=new JButton("登陆");  
+		b_log.addActionListener(new ActionListener() {  
+		    public void actionPerformed(ActionEvent e) {  
+		        // TODO Auto-generated method stub  
+		        //获取用户名和密码，进行校验  
+		        String myUsername=tf_name.getText(); 
+				String myPassword=password.getText();  
+		        if(myUsername.equals("admin")&& myPassword.equals("123456")){  
+		            JOptionPane.showMessageDialog(null, "登陆成功!"); 
+		            new MyExGUI();
+		            //graph dati = new graph();
+		            //dati.exercise();
+		            //Answer an =new Answer();
+		            //an.show();
+		            f.dispose();
+		            //              System.exit(0);  
+		
+		        }else{  
+		            JOptionPane.showMessageDialog(null, "账号或密码错误!");  
+		            name.setText("");  
+		            password.setText("");  
+		
+		        }  
+		          
+		    }  
+		});  
+		pan4.add(b_log);  
+		  
+		
+		JButton b_exit=new JButton("退出");  
+		pan4.add(b_exit);   
+		con.add(pan4);  
+		//登陆和退出这两个按钮放在第四个版面上
+		
+		JPanel pan5 = new JPanel();
+		con.add(pan5);
+		JPanel pan6 = new JPanel();
+		con.add(pan6);
+		JPanel pan7 = new JPanel();
+		con.add(pan7);
+		//空白版面
 	}
-	
-*/
+
 }
--- src/ui_test/MyExGUI.java
+++ src/ui_test/MyExGUI.java
@@ -1,25 +1,35 @@
 package ui_test;
 
 import java.awt.*;  
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.*;  
 
 
 public class MyExGUI extends JFrame{
+	ArrayList<String> user_answer = new ArrayList<String>();
+	ArrayList<String> true_answer = new ArrayList<String>();
 	JMenuBar jmb;   //菜单条组件  
     JMenu menu1, menu2, menu3, menu4, menu5;//菜单  
     JMenuItem item1, item2, item3, item4, item5, item6;//菜单项  
     JMenu build;    //二级菜单  
-    JMenuItem file, project;  
-    JTextArea jta;  
-      
+    JMenuItem file, project; 
+    TextArea answer_all = new TextArea();
+    TextField jta = new TextField();
+    TextField jta_answer = new TextField(); 
+    JLabel num_answer = new JLabel();
+    JLabel answer1,answer2,answer3,answer4,answer5,answer6,answer7,answer8,answer9,answer10,
+    answer11,answer12,answer13,answer14,answer15,answer16,answer17,answer18,answer19,answer20;
     JToolBar jtb;//工具条  
-    JButton jb1, jb2, jb3, jb4, jb5, jb6, jb7;  
-      
+    JButton jb1, jb2, jb3, jb4, jb5, jb6, jb7,jb_next;  
+    int answer_count;
     public MyExGUI()
     {  
         //创建菜单  
         jmb = new JMenuBar();  
-          
+        
         menu1 = new JMenu("文件(F)");  
         menu1.setMnemonic('f'); //助记符  
         menu2 = new JMenu("编辑");  
@@ -31,15 +41,36 @@ public class MyExGUI extends JFrame{
         build = new JMenu("新建");  
           
         file = new JMenuItem("文件");  
-        project = new JMenuItem("工程");  
+        project = new JMenuItem("答题");  
         item1 = new JMenuItem("打开");  
         item2 = new JMenuItem("保存(S)");  
         item3 = new JMenuItem("另存为");  
         item4 = new JMenuItem("页面设置");  
         item5 = new JMenuItem("打印");  
         item6 = new JMenuItem("退出");  
+        
+        answer1 = new JLabel("第  1 题");
+        answer2= new JLabel("第二题");
+        answer3= new JLabel("第三题");
+        answer4= new JLabel("第四题");
+        answer5= new JLabel("第五题");
+        answer6= new JLabel("第六题");
+        answer7= new JLabel("第七题");
+        answer8= new JLabel("第八题");
+        answer9= new JLabel("第九题");
+        answer10= new JLabel("第十题");
+        answer11= new JLabel("第十一题");
+        answer12= new JLabel("第十二题");
+        answer13= new JLabel("第十三题");
+        answer14= new JLabel("第十四题");
+        answer15= new JLabel("第十五题");
+        answer16= new JLabel("第十六题");
+        answer17= new JLabel("第十七题");
+        answer18= new JLabel("第十八题");
+        answer19= new JLabel("第十九题");
+        answer20= new JLabel("第二十题");
           
-        jta = new JTextArea();  
+        
           
             //添加菜单项至菜单上  
         build.add(file);  
@@ -59,33 +90,108 @@ public class MyExGUI extends JFrame{
         jmb.add(menu3);  
         jmb.add(menu4);  
         jmb.add(menu5);  
-        //创建工具条  
-        jtb = new JToolBar();  
-        jb1 = new JButton("新建");  
-        jb1.setToolTipText("新建");//鼠标悬停提示信息  
-        jb2 = new JButton("打开");  
-        jb3 = new JButton("复制");  
-        jb4 = new JButton("剪切");  
-        jb5 = new JButton("打印");  
-        jtb.add(jb1);  
-        jtb.add(jb2);  
-        jtb.add(jb3);  
-        jtb.add(jb4);  
-        jtb.add(jb5);  
-        jta = new JTextArea();  
-            //加入滚动条  
-        //JScrollPane jsp = new JScrollPane(jta);  
-        //jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);  
           
+        
+    	JPanel contentPanel = new JPanel();
+    	contentPanel.setLayout(null);
+    	JLabel daan = new JLabel("答案");
+    	JLabel dengyu = new JLabel("=");
+        num_answer=answer1;
+        num_answer.setFont(new Font("宋体",Font.BOLD, 22));
+        jb_next = new JButton("下一题");
+        jta.setFont(new Font("宋体",Font.BOLD, 22));
+        jta_answer.setFont(new Font("宋体",Font.BOLD, 22));
+        jb_next.setFont(new Font("宋体",Font.BOLD, 22));
+        daan.setFont(new Font("宋体",Font.BOLD, 22));
+        dengyu.setFont(new Font("宋体",Font.BOLD, 22));
+        
+    	contentPanel.add(num_answer);
+    	contentPanel.add(daan);
+    	contentPanel.add(dengyu);
+    	contentPanel.add(jta);
+
+    	contentPanel.add(jta_answer);
+    	contentPanel.add(answer_all);
+    	contentPanel.add(jb_next);
+    	
+        num_answer.setBounds(90, 20, 90, 50);
+        daan.setBounds(250, 20, 90, 50);
+        jta.setBounds(50, 70, 150, 30);
+        dengyu.setBounds(205, 70, 20, 20);
+        jta_answer.setBounds(230, 70, 100, 30);
+        jb_next.setBounds(350, 70, 110, 30);
+        answer_all.setBounds(50, 120, 400, 300);
+        
         this.setJMenuBar(jmb);  //添加菜单栏，不能设定位置，会自动放在最上部  
-        //this.add(jtb, BorderLayout.NORTH);  //添加工具条  
-        //this.add(jsp, BorderLayout.CENTER); //添加文本域  
-          
-        this.setTitle("NotePad");  
+        this.add(contentPanel);
+        
+        this.setTitle("在线答题系统");  
         this.setSize(600, 500);  
         this.setVisible(true);  
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        project.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				arithmetic art = new arithmetic();
+				true_answer=art.list_answer;
+				answer_count=0;
+				for (int i = 0; i < art.list_timu.size(); i++)
+				{
+					answer_all.append(art.list_timu.get(i));
+					answer_all.append("\r\n");
+				}
+				jta.setText(art.list_timu.get(answer_count));
+				answer_count++;
+				
+				
+			}
+		});
+        jb_next.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String temp;
+				temp = jta_answer.getText();
+				if(isInteger(temp))
+				{
+					
+					user_answer.add(temp);
+					num_answer.setText("第  "+answer_count+" 题");
+					if(answer_count<20)
+						answer_count++;
+					else{
+						
+						Object[] options = { "是", "取消" }; 
+						JOptionPane.showOptionDialog(null, "点击以继续 查看成绩", "答题完毕", 
+						JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, 
+						null, options, options[0]); 
+						
+					}
+					
+				}
+				
+				
+			}
+		});
+        
+        
+        
+        
     }
+    
+    public static boolean isInteger(String str) {    
+	    for (int i = str.length();--i>=0;){  
+			        if (!Character.isDigit(str.charAt(i))){
+			            return false;
+			        }
+			    }
+			    return true;
+	  } 
+    
+    
 	
 	
 }
